f8dd68fcaeabbb4eef9a365f8a49b33bef66d4bb
Matias Woloski <matiasw@gmail.com>
2012-05-25 18:02:36 -0300

    change server

--- apps/hello2/server.js
+++ apps/hello2/server.js
@@ -1,26 +1,38 @@
-var http = require('http'),
-	https = require('https');
-	fs = require('fs');
+var fs = require('fs'),
+    https = require('https'),
     httpProxy = require('http-proxy');
 
 var options = {
   https: {
-    key: fs.readFileSync('mygoogle.com.key.pem', 'utf8'),
-    cert: fs.readFileSync('mygoogle.com.certificate.pem', 'utf8')
+    key: fs.readFileSync('./mygoogle.com.key.pem', 'utf8'),
+    cert: fs.readFileSync('./mygoogle.com.certificate.pem', 'utf8')
   },
   target: {
     https: true // This could also be an Object with key and cert properties
   }
 };
 
+
+//
+// Create an instance of HttpProxy to use with another HTTPS server
+//
 var proxy = new httpProxy.HttpProxy({ 
-	  target: {
-	    host: 'google.com', 
-	    port: 80,
-	    https: true
-	  }
-	});
+  target: {
+    host: 'localhost', 
+    port: 8000,
+    https: true
+  }
+});
 
 https.createServer(options.https, function (req, res) {
   proxy.proxyRequest(req, res);
-}).listen(process.env.PORT || 9000);
+}).listen(process.env.PORT || 8002);
+
+//
+// Create the target HTTPS server for both cases
+//
+https.createServer(options.https, function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('hello https\n');
+  res.end();
+}).listen(8000);
