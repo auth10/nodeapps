b0ffaec2e7d12916f62efc759263c17f2b4aa64e
David Frassoni <david.frassoni@gmail.com>
2012-06-14 00:47:08 -0300

    - Updated Pictionary app with latest version

--- apps/pictionary/server.js
+++ apps/pictionary/server.js
@@ -1,44 +1,80 @@
 var app = require('http').createServer(handler);
-var socketio = require('socket.io');
+var io = require('socket.io').listen(app, { log: false });
 var fs = require('fs');
 var sanitizer = require('sanitizer');
 
 var port = process.env.PORT || 7000;
 app.listen(port);
 console.log('server listening on ' + port);
-var io = socketio.listen(app, { log: true });
-console.log('socket.io started');
+
+// ================================================
+//                           server routing section
+// ================================================
 
 function handler (req, res) {
-	fs.readFile(__dirname + '/client' + req.url,
-	function (err, data) {
-		if (err) {
-			res.writeHead(500);
-			return res.end('Error loading requested file ' + req.url);
+	var reqFile = req.url;
+	
+	if (reqFile == "/") {
+		reqFile = "/index.html";
+	}
+	
+	fs.readFile(__dirname + '/client' + reqFile,
+		function (err, data) {
+			if (err) {
+				res.writeHead(200);
+				return res.end('Error loading requested file ' + reqFile);
+			}
+			
+			var filetype = reqFile.substring(reqFile.lastIndexOf('.'), reqFile.length);
+			switch(filetype) {
+				case ".html":
+					res.setHeader("Content-Type", "text/html");
+					break;
+				case ".js":
+					res.setHeader("Content-Type", "application/javascript");
+					break;
+				case ".css":
+					res.setHeader("Content-Type", "text/css");
+					break;
+				case ".gif":
+					res.setHeader("Content-Type", "image/gif");
+					break;
+				case ".png":
+					res.setHeader("Content-Type", "image/png");
+					break;
+			}
+			
+			res.writeHead(200);
+			res.end(data);
 		}
-
-		res.writeHead(200);
-		res.end(data);
-	});
+	);
 }
+
+// ================================================
+//                                app logic section
+// ================================================
 
 var users = [], canvas = [];
 var dictionary, currentWord, currentPlayer;
 
-// Load words dictionary into memory
+// load dictionary.txt into memory
 fs.readFile(__dirname + '/dictionary.txt', function (err, data) {
 	dictionary = data.toString('utf-8').split("\r\n");
 });
 
-// User connections
 io.sockets.on('connection', function (socket) {
-	var myNick = 'guest';
-	var myColor = rndColor();
+	var myNick = 'guest',
+		myColor = rndColor();
+		myScore = 0;
 	
-	users.push({ id: socket.id, nick: myNick, color: myColor });
+	users.push({ id: socket.id, nick: myNick, color: myColor, score: myScore });
 	io.sockets.emit('userJoined', { nick: myNick, color: myColor });
 	io.sockets.emit('users', users);
 	socket.emit('drawCanvas', canvas);
+	
+	// =============
+	// chat logic section
+	// =============
 	
 	socket.on('message', function (msg) {
 		var sanitizedMsg = sanitizer.sanitize(msg.text);
@@ -50,6 +86,24 @@ io.sockets.on('connection', function (socket) {
 		}
 		
 		io.sockets.emit('message', { text: sanitizedMsg, color: myColor, nick: myNick });
+		
+		// check if current word was guessed
+		if(currentPlayer != null && currentPlayer != socket.id) {
+			if(sanitizedMsg == currentWord) {
+				io.sockets.emit('wordGuessed', { text: sanitizedMsg, color: myColor, nick: myNick });
+				
+				for(var i = 0; i<users.length; i++) {
+					if(users[i].id == socket.id || users[i].id == currentPlayer) {
+						users[i].score = users[i].score + 10;
+					}
+				}
+				
+				currentPlayer = null;
+				sortUsersByScore();
+				io.sockets.emit('users', users);
+				io.sockets.emit('youCanDraw');
+			}
+		}
 	});
 	
 	socket.on('nickChange', function (user) {
@@ -87,18 +141,35 @@ io.sockets.on('connection', function (socket) {
 		
 		if(currentPlayer == socket.id) {
 			currentPlayer = null;
-			// emit : ALLOW USER DRAW
+			io.sockets.emit('youCanDraw');
 		}
 	});
 	
 	socket.on('draw', function (line) {
-		canvas.push(line);
-		socket.broadcast.emit('draw', line);
+		if(currentPlayer == socket.id) {
+			canvas.push(line);
+			socket.broadcast.emit('draw', line);
+		}
 	});
 	
 	socket.on('clearCanvas', function () {
-		canvas.splice(0, canvas.length);
-		io.sockets.emit('clearCanvas');
+		if(currentPlayer == socket.id) {
+			canvas.splice(0, canvas.length);
+			io.sockets.emit('clearCanvas');
+		}
+	});
+	
+	socket.on('changeNickColor', function() {
+		myColor = rndColor();
+		
+		for(var i = 0; i<users.length; i++) {
+			if(users[i].id == socket.id) {
+				users[i].color = myColor;
+				break;
+			}
+		}
+		
+		io.sockets.emit('users', users);
 	});
 	
 	function rndColor() {
@@ -106,18 +177,27 @@ io.sockets.on('connection', function (socket) {
 		return color;
 	};
 	
-	// ================================================
-	//                           pictionary logic section
-	// ================================================
+	function sortUsersByScore() {
+		users.sort(function(a,b) { return parseFloat(b.score) - parseFloat(a.score) } );
+	}
+	
+	// =================
+	// pictionary logic section
+	// =================
 	
 	socket.on('readyToDraw', function () {
 		if (!currentPlayer) {
 			currentPlayer = socket.id;
-			var randomLine = Math.floor(Math.random() * dictionary.length);
-			var line = dictionary[randomLine];
-			var word = line.split(",");
+			canvas.splice(0, canvas.length);
+			io.sockets.emit('clearCanvas');
+			
+			var randomLine = Math.floor(Math.random() * dictionary.length),
+				line = dictionary[randomLine],
+				word = line.split(",");
+			
+			currentWord = word[0];
 			socket.emit('youDraw', word);
-			io.sockets.emit('drawingTime', { color: myColor, nick: myNick });
+			io.sockets.emit('firendDraw', { color: myColor, nick: myNick });
 		}
 	});
 });
